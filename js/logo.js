201340615fa6bea4bd80b21545f64e6ebc055a04
chrisvasey <chrismvasey@gmail.com>
2014-09-23 00:05:49 +0100

    started animating logo

---
+++ js/logo.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#lxone").addClass('lone');	
+	$("#lxtwo").delay(1000).addClass('ltwo');
+	$("#lxthree").delay(1000).addClass('lthree');
+	$("#lxfour").delay(1000).addClass('lfour');
+	$("#lxfive").delay(1000).addClass('lfive');
+	$("#lxsix").delay(1000).addClass('lsix');
+});
+
